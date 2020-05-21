cb9e040ade1d711ef10456b14afd3e894afb9777
intensifies <aintensifies@gmail.com>
2020-05-21 18:59:00 +0700

    fix: outer subtitles

--- src/screens/MovieScreen/index.tsx
+++ src/screens/MovieScreen/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
 export const MovieScreen = () => {
+  const ref = useRef<HTMLVideoElement>(null)
+  const [sub, setSub] = useState<string | undefined>()
+
+  const handlePlay = () => {
+    ref.current?.play().then(() => {
+      const subtitles = ref.current?.textTracks[0]
+      const handleCueChange = function (this: TextTrack): void {
+        setSub(this?.activeCues?.[0]?.text)
+      }
+
+      if (subtitles) {
+        subtitles.oncuechange = handleCueChange
+      }
+    })
+  }
+
+  const handlePause = () => {
+    ref.current?.pause()
+  }
+
   return (
     <div>
-      <video controls width="800" height="500">
+      <video width="800" height="500" ref={ref}>
         <source
           src="https://www1872.o0-4.com/token=AuB8u3K5VnH2Rm8YyYz2cQ/1590071396/94.180.0.0/29/c/4e/4b9421e7a1f208e0d8b110e54bbab4ec-360p.mp4"
           type="video/mp4"
@@ -15,6 +35,9 @@ export const MovieScreen = () => {
           srcLang="en"
         ></track>
       </video>
+      <div onClick={handlePlay} />
+      <div onClick={handlePause} />
+      <div>{sub}</div>
     </div>
   )
 }
