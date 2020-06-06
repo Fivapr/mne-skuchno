ba40a2fbb8a85106f32db1568a82996c76e4b81b
intensifies <aintensifies@gmail.com>
2020-06-06 12:15:04 +0700

    feat: локальные файлы

--- src/components/SrcInput/index.tsx
+++ src/components/SrcInput/index.tsx
@@ -1,25 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState, ChangeEvent } from "react";
 
 interface Props {
-  onButtonClick: (value: string) => void
+  onButtonClick: (value: string) => void;
 }
 
+// setVideoSource (file) {
+//   const previewReader = new FileReader();
+//   previewReader.readAsDataURL(file);
+//   previewReader.onloadend = () => {
+//    const dataUri = previewReader.result;
+//    const blob = dataURItoBlob(dataUri);
+//    const videoSource = URL.createObjectURL(blob);
+//    this.setState({ videoSource });
+//   };
+//  }
+
 export const SrcInput = (props: Props) => {
-  const [src, setSrc] = useState('')
+  const [src, setSrc] = useState("");
 
   const handleClick = () => {
-    props.onButtonClick(src)
-  }
+    props.onButtonClick(src);
+  };
 
   return (
     <>
       <input
+        type="file"
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          if (e.target.files!.length) {
+            setSrc(URL.createObjectURL(e.target.files![0]));
+          }
+        }}
+      />
+      <input
         value={src}
-        onChange={(e: any) => {
-          setSrc(e.target.value)
+        onChange={(e) => {
+          setSrc(e.currentTarget.value);
         }}
       />
       <button onClick={handleClick}>set Src</button>
     </>
-  )
-}
+  );
+};
